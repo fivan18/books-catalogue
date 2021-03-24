887a66bfbc1c1bfb2d529910ffa69e7947acc540
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-03-23 20:55:20 -0600

    add person list component to test axios http client

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import './homepage.styles.scss';
 
+import PersonList from '../../components/person-list/person-list.component';
+
 const HomePage = () => (
   <div className="home-page">
-    Home Page
+    <PersonList />
   </div>
 );
 
