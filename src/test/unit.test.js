f542974ebd612730eedc95b8d3e1be5430adf8a5
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-04-14 20:37:56 -0500

    fix eslint errors

--- src/test/unit.test.js
+++ src/test/unit.test.js
@@ -2,12 +2,12 @@ import { choseBookByKey } from '../redux/book/book.utils';
 import { mockBooks } from './mock.data';
 
 describe('unit test', () => {
-    test('choseBookByKey: get an existing book', () => {
-       expect(choseBookByKey(mockBooks, '/works/OL17499975W'))
-            .toEqual(mockBooks[0]);
-    });
-    test('choseBookByKey: get a nonexisting book', () => {
-        expect(choseBookByKey(mockBooks, '/works/OL123578W'))
-            .toBeUndefined()
-    });
+  test('choseBookByKey: get an existing book', () => {
+    expect(choseBookByKey(mockBooks, '/works/OL17499975W'))
+      .toEqual(mockBooks[0]);
+  });
+  test('choseBookByKey: get a nonexisting book', () => {
+    expect(choseBookByKey(mockBooks, '/works/OL123578W'))
+      .toBeUndefined();
+  });
 });
